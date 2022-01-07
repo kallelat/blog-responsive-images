e734387ebda70b68cd840a305f049a2afa184970
Timo Kallela <timo.kallela@gmail.com>
2022-01-07 11:48:30 +0200

    improve code

--- src/components/picture.tsx
+++ src/components/picture.tsx
@@ -12,15 +12,15 @@ const Picture = () => {
         You can finetune the media queries however you like, you can for example
         use different image for portrait and landscape versions.
       */}
-      <source media="(max-width: 480px)" srcSet="/images/scenery-480w.jpg" />
-      <source media="(max-width: 800px)" srcSet="/images/scenery-800w.jpg" />
+      <source media="(max-width: 480px)" srcSet="scenery-480w.jpg" />
+      <source media="(max-width: 800px)" srcSet="scenery-800w.jpg" />
 
       {/*
         If none of the <source> elements return a match, then the browser will use normal <img> here.
         This <img> will be also used on the browsers that do not support <picture>.
       */}
       <img
-        src="/images/scenery.jpg"
+        src="scenery.jpg"
         alt="A beautiful yet a little heavy to load a mountain scenery"
       />
     </picture>
