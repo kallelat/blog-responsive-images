32c39df2ee4aee785d820a814bdbf82c00fa0be9
Timo Kallela <timo.kallela@gmail.com>
2022-01-07 11:46:50 +0200

    improved code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./components/header";
+import Picture from "./components/picture";
 import Text from "./components/text";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Picture />
       <Text />
     </div>
   );
---
+++ src/components/picture.tsx
@@ -0,0 +1,12 @@
+import "./picture.css";
+
+const Picture = () => {
+  return (
+    <img
+      src="scenery.jpg"
+      alt="A beautiful yet a little heavy to load a mountain scenery"
+    />
+  );
+};
+
+export default Picture;
